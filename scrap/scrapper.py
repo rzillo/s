29957e52558554ce352a3c1829a763225a75291b
rzillo <42211951+rzillo@users.noreply.github.com>
2023-08-30 23:26:33 -0300

    Update scrapper.py
    idk

--- scrap/scrapper.py
+++ scrap/scrapper.py
@@ -2,7 +2,7 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import requests
 
-#maybe other links
+#try links
 link = 'https://www.amazon.com.br/MSI-Gaming-RTX-3080-Trio/dp/B095VZ6F73'
 r = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 open_file = urlopen(r)
