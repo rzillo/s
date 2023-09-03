ed6fec4794cd1e03749381a17d406af219032a7b
rzillo <42211951+rzillo@users.noreply.github.com>
2023-09-02 22:36:43 -0300

    Update ds.py

--- ds/ds.py
+++ ds/ds.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 a = np.array([0, 1, 2, 3, 4, 5, 6])
-#math 
+#math and better calculations
 error = 1/n * np.sum(np.square(predictions - labels))
 
 b = np.array([[-2.58289208,  0.43014843, -1.24082018, 1.59572603],
