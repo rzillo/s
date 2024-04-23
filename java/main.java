d839230a17115c3fb12d0cb02d6a9e51aefb8816
rzillo <42211951+rzillo@users.noreply.github.com>
2024-04-22 23:10:25 -0300

    Update main.java

--- java/main.java
+++ java/main.java
@@ -3,8 +3,13 @@ public class MyClass {
         
         int max = Integer.MAX_VALUE;
         int min = Integer.MIN_VALUE;
+
+        float maxf = Float.MAX_VALUE;
+        float minf = Float.MIN_VALUE;
         
-      System.out.println(max);
-      System.out.println(min);
+        System.out.println(max);     
+        System.out.println(min);
+        System.out.println(maxf);
+        System.out.println(minf);
     }
 }
